6afc35403432e4fcc904b1cd2fb6a6023e2350eb
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-11-19 20:06:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-themeButton.onclick = function() {
-  page.classList.toggle('light-theme');
-  page.classList.toggle('dark-theme');
+let list = document.querySelector('.todo-list');
+let input = document.querySelector('.todo-input');
+let form = document.querySelector('.todo-form');
+let priority = document.querySelector('.todo-priority');
+
+priority.onclick = function () {
+  priority.classList.toggle('is-important');
+  if (priority.classList.contains('is-important')) {
+    priority.textContent = 'Важная задача';
+  } else {
+    priority.textContent = 'Обычная задача';
+  }
 };
 
-let message = document.querySelector('.subscription-message');
-let form = document.querySelector('.subscription');
-let email = document.querySelector('.subscription-email');
-
-form.onsubmit = function(evt) {
+form.onsubmit = function (evt) {
   evt.preventDefault();
-  // Измените значение textContent на следующей строке
-  message.textContent = 'Адрес ' + email.value + ' добавлен в список получателей рассылки.';
+ let newTask = document.createElement('li');
+ newTask.textContent = input.value;
+ if (priority.classList.contains('is-important')) {
+   newTask.classList.add('is-important');
+   }
+ list.append(newTask);
+ input.value = '';
 };
