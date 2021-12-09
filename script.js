3dcacf3aa125bcf60d069a3be7169c73311f54a2
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-09 23:18:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let upButton = document.querySelector('.up-button');
+let totalPages = 6; // Всего страниц в документе
 
-window.onscroll = function () {
-  if (window.pageYOffset > 200) {
-    upButton.classList.add('shown');
+let consumptionTotal = 0; // Общий расход тонера
+let consumptionPerPage = 70; // Расход краски на одну страницу
+let economyMode = true;
+
+for (let page = 1; page <= totalPages; page++) {
+  keks.print(page);
+
+  if (economyMode && page > 3) {
+    consumptionTotal += consumptionPerPage * 0.5;
   } else {
-    upButton.classList.remove('shown');
+  consumptionTotal += consumptionPerPage;
   }
+  console.log(consumptionTotal);
+}
 
-};
-
-upButton.onclick = function () {
-  window.scrollTo(0, 0);
-};
