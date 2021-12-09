720c02eab68b4e6734105fdd3e5b93a3fa9f7def
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-09 23:18:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+let mode = 'alternate'; // Режим работы драйвера печати
+let pageNumber = 5; // Номер копируемой страницы
+let copyCount = 7; // Количество копий
 let totalPages = 6; // Всего страниц в документе
+let startPage = 2; // Стартовая страница
 
-let consumptionTotal = 0; // Общий расход тонера
-let consumptionPerPage = 70; // Расход краски на одну страницу
-let economyMode = true;
-
-for (let page = 1; page <= totalPages; page++) {
-  keks.print(page);
-
-  if (economyMode && page > 3) {
-    consumptionTotal += consumptionPerPage * 0.5;
-  } else {
-  consumptionTotal += consumptionPerPage;
+if (mode === 'pageCopy') {
+  for (let copies = 1; copies <= copyCount; copies++) {
+    keks.print(pageNumber);
   }
-  console.log(consumptionTotal);
 }
 
+if (mode === 'document') {
+  for (let page = 1; page <= totalPages; page++) {
+    keks.print(page);
+  }
+}
+
+if (mode === 'reverse') {
+  for (let reversePage = totalPages; reversePage >= 1; reversePage--) {
+    keks.print(reversePage);
+  }
+}
+
+if (mode === 'alternate') {
+  for (let alternatePage = startPage; alternatePage <= totalPages; alternatePage += 2) {
+    keks.print(alternatePage);
+  }
+}
